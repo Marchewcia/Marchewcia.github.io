f74c1c6813503376f3b67c0b9e26e764017b3483
Marchewcia <michalgolasinski6@gmail.com>
2023-12-05 23:34:03 +0100

    minor changes

--- script.php
+++ script.php
@@ -15,6 +15,103 @@ $odp13=$_POST["q13"];
 $odp14=$_POST["q14"];
 $odp15=$_POST["q15"];
 $odp16=$_POST["q16"];
+
 $suma=0;
-if()
+
+if($odp1="c"){
+    $suma++;
+    echo "<p>Pytanie 1-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 1-niepoprawna odpowiedź</p>";
+}
+
+if($odp2="a"){
+    $suma++;
+    echo "<p>Pytanie 2-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 2-niepoprawna odpowiedź</p>";
+}
+
+if($odp3="c"){
+    $suma++;
+    echo "<p>Pytanie 3-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 3-niepoprawna odpowiedź</p>";
+}
+
+if($odp4="d"){
+    $suma++;
+    echo "<p>Pytanie 4-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 4-niepoprawna odpowiedź</p>";
+}
+
+if($odp5="c"){
+    $suma++;
+    echo "<p>Pytanie 5-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 5-niepoprawna odpowiedź</p>";
+}
+
+if($odp6="b"){
+    $suma++;
+    echo "<p>Pytanie 6-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 6-niepoprawna odpowiedź</p>";
+}
+
+if($odp7="b"){
+    $suma++;
+    echo "<p>Pytanie 7-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 7-niepoprawna odpowiedź</p>";
+}
+
+if($odp8="a"){
+    $suma++;
+    echo "<p>Pytanie 8-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 8-niepoprawna odpowiedź</p>";
+}
+
+if($odp9="d"){
+    $suma++;
+    echo "<p>Pytanie 9-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 9-niepoprawna odpowiedź</p>";
+}
+
+if($odp10=""){
+    $suma++;
+    echo "<p>Pytanie 10-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 10-niepoprawna odpowiedź</p>";
+}
+
+if($odp11=""){
+    $suma++;
+    echo "<p>Pytanie 11-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 11-niepoprawna odpowiedź</p>";
+}
+
+if($odp12=""){
+    $suma++;
+    echo "<p>Pytanie 12-poprawna odpowiedź(c)</p>";
+}
+else{
+    echo "<p>Pytanie 12-niepoprawna odpowiedź</p>";
+}
+
 ?>
